84e12621a8fa6573c5bd86f9c63b1a5b06031d95
ankurkumar1 <ankur.kumar1@xpressbees.com>
2023-07-08 00:21:16 +0530

    episode1 raw react code

---
+++ App.js
@@ -0,0 +1,15 @@
+// const header = document.createElement('h1');
+// header.innerHTML = "Hello world from JS"
+// const element = document.getElementById('root');
+// element.appendChild(header);
+
+
+// const heading = React.createElement("h1",{id:"heading"},"Hello world from react");
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading);
+const h1tag = React.createElement("h1",{}, "this is H1 tag");   // creates Object
+const h1tag2 = React.createElement("h1",{}, "this is another H1 tag")
+const child = React.createElement("div", { id: "child" }, [h1tag,h1tag2] ); // for sibling use array
+const parent = React.createElement("div", { id: "parent" }, child);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);    // converts react object to html elements(tags)
